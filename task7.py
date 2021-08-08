2bbe546826db1b0d7420a3fa38d68e3f7d2ac5dd
nisa2703 <85580989+nisa2703@users.noreply.github.com>
2021-08-08 23:16:53 +0530

    Add files via upload

---
+++ task7.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import subprocess
+import cgi
+
+print("content-type: text/html")
+print()
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+
+o = subprocess.getoutput("sudo " + cmd)
+print(o)
+
+
+
+
